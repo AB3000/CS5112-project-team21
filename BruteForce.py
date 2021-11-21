9c55788fdc239679c1c8a6e1558c1c385292e0c8
AB3000 <aarushban@gmail.com>
2021-11-20 22:22:52 -0500

    Added some brute force code (from geeksforgeeks)

---
+++ BruteForce.py
@@ -0,0 +1,27 @@
+
+def brute_force_search(pat, txt):
+    print('test')
+    
+    M = len(pat)
+    N = len(txt)
+ 
+    # A loop to slide pat[] one by one */
+    for i in range(N - M + 1):
+        j = 0
+         
+        # For current index i, check
+        # for pattern match */
+        while(j < M):
+            if (txt[i + j] != pat[j]):
+                break
+            j += 1
+ 
+        if (j == M):
+            print("Pattern found at index ", i)
+
+
+if __name__ == "__main__":
+
+    file = open('chimpanzee.txt', 'r')
+    text = file.read()
+
